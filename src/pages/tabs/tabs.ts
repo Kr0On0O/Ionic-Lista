398d023f25d46973ccc377ea36e73091d0f74cb0
Alvaro <alvaro.garcia.asmar@gmail.com>
2018-02-01 11:14:31 +0100

    Tercer commit

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import {PendientesComponent} from '../pendientes/pendientes.component';
+import {TerminadosComponent}  from '../terminados/terminados.component';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
@@ -9,9 +11,9 @@ import { HomePage } from '../home/home';
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = PendientesComponent;
+  tab2Root = TerminadosComponent;
+
 
   constructor() {
 
